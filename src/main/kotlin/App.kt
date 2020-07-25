134219193d2070cd018427c1234a789fd9758402
Felix Loehring <loehfelix@gmail.com>
2020-07-25 19:46:23 +0200

    refactored video lists from file level into App State

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -9,9 +9,26 @@ import styled.styledH1
 
 external interface AppState : RState {
     var currentVideo: VideoModel?
+    var unwatchedVideos: List<VideoModel>
+    var watchedVideos: List<VideoModel>
 }
 
 class App : RComponent<RProps, AppState>() {
+
+    // Override init method from state to fill state with predefined values
+    override fun AppState.init() {
+        unwatchedVideos = listOf(
+            VideoModel(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+            VideoModel(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+            VideoModel(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+        )
+
+        watchedVideos = listOf(
+            VideoModel(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+        )
+
+    }
+
     override fun RBuilder.render() {
 //      h1 is really a function that takes a lambda parameter.
 //      When we write +, we are really invoking the function unaryPlus (through operator overloading)
@@ -30,7 +47,7 @@ class App : RComponent<RProps, AppState>() {
             }
 
             videoList {
-                videos = unwatchedVideos
+                videos = state.unwatchedVideos
                 selectedVideo = state.currentVideo
                 onSelectVideo = { video -> setState { currentVideo = video } }
             }
@@ -39,7 +56,7 @@ class App : RComponent<RProps, AppState>() {
                 +"Videos watched"
             }
             videoList {
-                videos = watchedVideos
+                videos = state.watchedVideos
                 selectedVideo = state.currentVideo
                 onSelectVideo = { video -> setState { currentVideo = video } }
             }
@@ -49,6 +66,10 @@ class App : RComponent<RProps, AppState>() {
         state.currentVideo?.let { currentVideo ->
             videoPlayer {
                 video = currentVideo
+                unwatchedVideo = video in state.unwatchedVideos
+                onWatchedButtonPressed = { video ->
+                    throw NotImplementedError()
+                }
             }
         }
     }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,6 @@
 import react.dom.*
 import kotlin.browser.document
 
-val unwatchedVideos = listOf(
-    VideoModel(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-    VideoModel(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-    VideoModel(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
-)
-
-val watchedVideos = listOf(
-    VideoModel(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-)
-
 fun main() {
     render(document.getElementById("root")) {
         // Tell React to render the App component as a child of root
